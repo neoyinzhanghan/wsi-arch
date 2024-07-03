cb25241d064226ef4b45709f8dfbaef33eb95dce
Neo Yin <yinzhang@berkeley.edu>
2024-07-03 00:06:10 -0700

    prepare data script for UNI features

--- wsiarch/data/prepare_data.py
+++ wsiarch/data/prepare_data.py
@@ -102,11 +102,29 @@ if __name__ == "__main__":
     #     "/Users/neo/Documents/MODS/wsiarch/tmp",
     # )
 
+    # folder_as_class(
+    #     folders=[
+    #         "/media/hdd1/neo/TCGA-LUAD_SimCLR_2024-03-14",
+    #         "/media/hdd1/neo/TCGA-LUSC_SimCLR_2024-04-08",
+    #     ],
+    #     wsi_dirs=["/media/ssd1/TCGA_WSI/TCGA-LUAD", "/media/ssd1/TCGA_WSI/TCGA-LUSC"],
+    #     save_dir="/media/hdd1/neo/LUAD-LUSC_FI",
+    # )
+
+    # folder_as_class(
+    #     folders=[
+    #         "/media/hdd1/neo/TCGA-LUAD_SimCLR_2024-03-14",
+    #         "/media/hdd1/neo/TCGA-LUSC_SimCLR_2024-04-08",
+    #     ],
+    #     wsi_dirs=["/media/ssd1/TCGA_WSI/TCGA-LUAD", "/media/ssd1/TCGA_WSI/TCGA-LUSC"],
+    #     save_dir="/media/hdd1/neo/LUAD-LUSC_FI",
+    # )
+
     folder_as_class(
         folders=[
-            "/media/hdd1/neo/TCGA-LUAD_SimCLR_2024-03-14",
-            "/media/hdd1/neo/TCGA-LUSC_SimCLR_2024-04-08",
+            "/media/hdd1/neo/TCGA-LUSC_UNI",
+            "/media/hdd1/neo/TCGA-LUAD_UNI",
         ],
         wsi_dirs=["/media/ssd1/TCGA_WSI/TCGA-LUAD", "/media/ssd1/TCGA_WSI/TCGA-LUSC"],
-        save_dir="/media/hdd1/neo/LUAD-LUSC_FI",
+        save_dir="/media/hdd1/neo/LUAD-LUSC_FI_UNI",
     )
